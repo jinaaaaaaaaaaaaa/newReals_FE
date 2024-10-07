0aae5e521cad79f0f4f22c45f9e6ae7b3c617d2f
yeona <aya813@naver.com>
2024-10-07 20:30:39 +0900

    #1 [feat] : 기초 세팅

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier'; // Prettier 플러그인 추가
+import eslintConfigPrettier from 'eslint-config-prettier'; // eslint-config-prettier 추가
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': 'warn',
+    },
+  },
+);
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+  return <div>newReals</div>;
+}
+
+export default App;
